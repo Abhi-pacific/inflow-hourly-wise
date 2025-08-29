4de6520c29057bab1d99b0135462465152c959fa
abhishek chauhan <abhishek28c@gmail.com>
2025-08-29 16:25:52 +0530

    MIS detailed dashboard added

--- Inflow.py
+++ Inflow.py
@@ -8,13 +8,18 @@ with col3:
 with col2:
     st.subheader(f'Hourly wise Inflow, Abandon & Chat Count Day-1📈')
 
-# Inflow file upload
-raw_file_uploaded = st.file_uploader('Please upload the Inflow file here')
-# Roaster file upload
-roaster_file_uploaded = st.file_uploader('Please upload the Roaster file here')
+c1 = st.container()
+c2 = st.container()
+c3 = st.container()
+
+with c1:
+    # Inflow file upload
+    raw_file_uploaded = st.file_uploader('Please upload the Inflow file here')
+    # Roaster file upload
+    roaster_file_uploaded = st.file_uploader('Please upload the Roaster file here')
 if raw_file_uploaded and roaster_file_uploaded:
-    st.success('Both file received successfully')
-    st.write('Initiating the process....')
+    with c1:
+        st.success('Both file received successfully')
     
     
     #Converting the uploaded file in the excel engine
@@ -27,8 +32,14 @@ if raw_file_uploaded and roaster_file_uploaded:
     raw_parsed = raw_file.parse('Main File')
     roaster_raw = roaster_file.parse('Main Roaster')
 
+    # storeing the length of the data for MIS dash board
+    overall_inflow = len(raw_parsed)
+
     # droping the NA values for handling the # error in the excel.
     raw_parsed.dropna(subset=['Case First advisor assign time'],inplace=True)
+
+    # total inflow variable created for the mis add details
+    total_inflow = len(raw_parsed)
 
     # Performing the feature engineering.
     raw_parsed['AHTold'] = raw_parsed['Case Closure Time'] - raw_parsed['Case First Response Time']
@@ -41,8 +52,48 @@ if raw_file_uploaded and roaster_file_uploaded:
     raw_parsed['Brand FRT'] = raw_parsed['Case First Response Time'] - raw_parsed['Case First advisor assign time']
     raw_parsed['Actual advisor'] = raw_parsed['Case First Assigned to Advisor'] == raw_parsed['Case Last Assigned to Advisor']
 
+    # Average BRAND FRT (Overall, Including Outliers)
+    avg_brand_frt = raw_parsed['Brand FRT'].mean().round('1s')
+    avg_brand_frt_count = len(raw_parsed)
+    avg_brand_frt = str(avg_brand_frt).split('days')[1].strip()
+
+    # Calculation for average TAT (ACT)
+    average_tat = raw_parsed['ACT'].mean()
+    average_tat = average_tat.round('1s')
+    average_tat = str(average_tat).split('days')[1].strip()
+
+    # Case TAT unique cases (Cases created Day-1)
+    tat_hours_smaller_4 = raw_parsed[(raw_parsed['ACT'].dt.components['hours'] < 4)]
+    tat_hours_sm_4 = tat_hours_smaller_4['ACT'].mean().round('1s')
+    tat_hours_sm_4 = str(tat_hours_sm_4).split('days')[1].strip()
+
+    # Serviceable from Unique Inflow
+
+    unique_inflow = raw_parsed[~(raw_parsed['Closed Reason'] == 'UNRESPONSIVE')]
+    len_unique_inflow = len(unique_inflow)
+
+    unique_inflow_rev = raw_parsed[(raw_parsed['Closed Reason'] == 'UNRESPONSIVE')]
+    len_unique_inflow_rev = len(unique_inflow_rev)
+
+
+    # created temp  data for BRAND FRT (Time) -SL Breached
+    new_raw_parsed = raw_parsed
+    new_raw_parsed = new_raw_parsed[(new_raw_parsed['Brand FRT'].dt.components.minutes < 1)]
+    new_raw_parsed_rev = raw_parsed[(raw_parsed['Brand FRT'].dt.components.minutes >= 1)]
+    new_raw_parsed_rev_len = new_raw_parsed_rev.__len__()
+    avg_brand_frt_sl = new_raw_parsed_rev['Brand FRT'].mean().round('1s')
+    avg_brand_frt_sl = str(avg_brand_frt_sl).split('days')[1].strip()
+
+    avg_brand_frt_outlier = new_raw_parsed['Brand FRT'].mean().round('1s')
+    avg_brand_frt_outlier_count = len(new_raw_parsed)
+    avg_brand_frt_outlier = str(avg_brand_frt_outlier)
+    avg_brand_frt_outlier = avg_brand_frt_outlier.split('days')[1].strip()
+
     # Removing unwanted columns from the roaster file
-    roaster_raw = roaster_raw[['OLms ID','Emp Name','TL Name','Shift']]
+    roaster_raw = roaster_raw[['OLms ID','Emp Name','TL Name','Shift','present_attendance']]
+
+    # count of present employees
+    present_emp = (roaster_raw['present_attendance'] == 'P').sum()
 
     # Performing the merge operation
     raw_parsed = raw_parsed.merge(
@@ -79,7 +130,8 @@ if raw_file_uploaded and roaster_file_uploaded:
     Inflow_hourly_abondon_pivot.reset_index(names='Time_1',inplace=True)
     Inflow_hourly_abondon_pivot.columns = ['Time_1','Abondon']
 
-    
+    # abandon_count 
+    abandon_count = Inflow_hourly_abondon_pivot['Abondon'].sum()
     
     #Merging the Inflow and abondon tables
 
@@ -125,7 +177,7 @@ if raw_file_uploaded and roaster_file_uploaded:
     average_hold_time = raw_parsed.pivot_table(index=raw_parsed['Case Creation time'].dt.hour.values,values='Avg hold time',aggfunc='mean')
 
     # round off the micro-seconds
-    average_hold_time['Avg hold time'] = average_hold_time['Avg hold time'].dt.round('S')
+    average_hold_time['Avg hold time'] = average_hold_time['Avg hold time'].dt.round('s')
     
     average_hold_time['Avg hold time'] = average_hold_time['Avg hold time'].astype(str).str.split('days').str[1]
     average_hold_time.index = [pd.to_datetime(str(h),format='%H').strftime('%I %p') for h in average_hold_time.index]
@@ -182,8 +234,152 @@ if raw_file_uploaded and roaster_file_uploaded:
 
     Inflow_hourly_count.drop('index',axis=1,inplace=True)
 
-    # Showing the data frame
 
-    st.dataframe(Inflow_hourly_count)
+    # Staring the MIS add details
+    with c2:
+        chat = pd.Series(['MTD Inflow Data till ','MTD Inflow Data till ','MTD Inflow Data till ',
+                    'Inflow Data','Inflow Data','Inflow Data','Inflow Data','Inflow Data',
+                    'Productivity','Productivity','Productivity','Productivity','Productivity','Productivity','Productivity',
+                    'Tagging',
+                    'Brand FRT','Brand FRT','Brand FRT','Brand FRT','Brand FRT',
+                    'Case TAT','Case TAT','Case TAT','Case TAT',
+                    'Repeat'
+                    ])
+        
+        Particular = pd.Series(['Total Overall Inflow (Including Serviceable Opportunities)','Total Inflow Handled','Total Abandon','Overall Inflow (Including Serviceable Opportunities)','Total Inflow','Serviceable from Unique Inflow','NRR from Unique Inflow (Unresponsive)','Abandon','Advisors Logged IN (X-L1)',
+                            'Productivity as per the Approved FTE (22 with 34% Shrinkage)','ADVISOR PRODUCTIVITY OVERALL - Overall Cases','ADVISOR PRODUCTIVITY OVERALL- Serviceable Cases',
+                            'Productivity as per the Actual HC with Actual Shrinkage','ACTUAL ADVISOR PRODUCTIVITY OVERALL - Overall Cases','ACTUAL ADVISOR PRODUCTIVITY OVERALL- Serviceable Cases',
+                            'Tagged Count on Service Portal (On Closed cases)','BRAND FRT (Overall, Including Outliers)','Brand FRT Unique Cases (Outliers Removed)',
+                            'BRAND FRT (Achievement)','BRAND FRT (Time) -SL Breached','BRAND FRT SL Breached case count','Case TAT  (Overall, Including Outliers)',
+                            'Case TAT unique cases (Cases created Day-1)','Case TAT (Achievement)','Case TAT  (Time)- SL Breached','Repeat (Customer Message Vs Case ID)']) 
+        
+        # created the dataframe 
+        mis_add = pd.DataFrame({ 'chat': chat, 'Particular': Particular })
+
+        # Asking user for the date to create new column in the dataframe
+        date = st.text_input(f'Enter the Date')
+        
+        #inserting the values in the dataframe
+        
+        mis_add.loc[
+        (mis_add['chat'] == 'Productivity') &
+        (mis_add['Particular'] == 'Productivity as per the Approved FTE (22 with 34% Shrinkage)'),
+        'Target'
+        ] = 15
+
+        mis_add.loc[
+        (mis_add['chat'] == 'Brand FRT') & (mis_add['Particular'] == 'Brand FRT Unique Cases (Outliers Removed)'),'Target'
+        ] = '< 1 Minutes'
+
+        mis_add.loc[
+        (mis_add['chat'] == 'Inflow Data') & (mis_add['Particular'] == 'Overall Inflow (Including Serviceable Opportunities)'),date
+        ] = overall_inflow
+
+        mis_add.loc[
+        (mis_add['chat'] == 'Inflow Data') & (mis_add['Particular'] == 'Total Inflow'),date
+        ] = total_inflow
+
+        mis_add.loc[
+        (mis_add['chat'] == 'Inflow Data') & (mis_add['Particular'] == 'Serviceable from Unique Inflow'),date
+        ] = len_unique_inflow
+
+        mis_add.loc[
+        (mis_add['chat'] == 'Inflow Data') & (mis_add['Particular'] == 'NRR from Unique Inflow (Unresponsive)'),date
+        ] = len_unique_inflow_rev
+
+        mis_add.loc[
+        (mis_add['chat'] == 'Inflow Data') & (mis_add['Particular'] == 'Abandon'),date
+        ] = abandon_count
+
+        mis_add.loc[
+        (mis_add['chat'] == 'Productivity') & (mis_add['Particular'] == 'Productivity as per the Approved FTE (22 with 34% Shrinkage)'),'Target'
+        ] = 15
+
+        mis_add.loc[
+        (mis_add['chat'] == 'Productivity') & (mis_add['Particular'] == 'Productivity as per the Approved FTE (22 with 34% Shrinkage)'),date
+        ] = (total_inflow/15).__round__(0)
+
+        mis_add.loc[
+        (mis_add['chat'] == 'Productivity') & (mis_add['Particular'] == 'ADVISOR PRODUCTIVITY OVERALL - Overall Cases'),date
+        ] = (total_inflow/15).__round__(0)
+
+        mis_add.loc[
+        (mis_add['chat'] == 'Productivity') & (mis_add['Particular'] == 'ADVISOR PRODUCTIVITY OVERALL- Serviceable Cases'),date
+        ] = (total_inflow/15).__round__(0)
+
+        mis_add.loc[
+        (mis_add['chat'] == 'Productivity') & (mis_add['Particular'] =='Advisors Logged IN (X-L1)'), date
+        ] = present_emp
+
+        mis_add.loc[
+        (mis_add['chat'] == 'Productivity') & (mis_add['Particular'] == 'Productivity as per the Actual HC with Actual Shrinkage'),date
+        ] = (total_inflow/present_emp).__round__()
+
+        mis_add.loc[
+        (mis_add['chat'] == 'Productivity') & (mis_add['Particular'] == 'ACTUAL ADVISOR PRODUCTIVITY OVERALL - Overall Cases'),date
+        ] = (total_inflow/present_emp).__round__()
+
+        mis_add.loc[
+        (mis_add['chat'] == 'Productivity') & (mis_add['Particular'] == 'ACTUAL ADVISOR PRODUCTIVITY OVERALL- Serviceable Cases'),date
+        ] = (total_inflow/present_emp).__round__()
+
+        mis_add.loc[
+        (mis_add['chat'] == 'Tagging') & (mis_add['Particular'] == 'Tagged Count on Service Portal (On Closed cases)'),date
+        ] = '100%'
+
+        mis_add.loc[ 
+        (mis_add['chat'] == 'Brand FRT') & (mis_add['Particular'] == 'BRAND FRT (Overall, Including Outliers)'),date
+        ] = avg_brand_frt
+
+        # "99.00%"
+        s = (avg_brand_frt_outlier_count/avg_brand_frt_count)
+        val_percent = f"{s * 100:.2f}%" 
+
+        mis_add.loc[ 
+        (mis_add['chat'] == 'Brand FRT') & (mis_add['Particular'] == 'Brand FRT Unique Cases (Outliers Removed)'),date
+        ] = val_percent
+
+        mis_add.loc[ 
+        (mis_add['chat'] == 'Brand FRT') & (mis_add['Particular'] == 'Brand FRT Unique Cases (Outliers Removed)'),'Target'
+        ] = '>95%'
+
+        mis_add.loc[ 
+        (mis_add['chat'] == 'Case TAT') & (mis_add['Particular'] == 'Case TAT unique cases (Cases created Day-1)'),'Target'
+        ] = '< 4 hours'
+
+        mis_add.loc[ 
+        (mis_add['chat'] == 'Case TAT') & (mis_add['Particular'] == 'Case TAT (Achievement)'),'Target'
+        ] = '> 90%'
+
+        mis_add.loc[ 
+        (mis_add['chat'] == 'Case TAT') & (mis_add['Particular'] == 'Case TAT (Achievement)'),date
+        ] = '100%'
+
+        mis_add.loc[ 
+        (mis_add['chat'] == 'Brand FRT') & (mis_add['Particular'] == 'BRAND FRT (Time) -SL Breached'),date
+        ] = avg_brand_frt_sl
+
+        mis_add.loc[ 
+        (mis_add['chat'] == 'Brand FRT') & (mis_add['Particular'] == 'BRAND FRT SL Breached case count'),date
+        ] = new_raw_parsed_rev_len
+
+        mis_add.loc[
+        (mis_add['chat'] == 'Case TAT') & (mis_add['Particular'] == 'Case TAT  (Overall, Including Outliers)'),date
+        ] = average_tat
+
+        mis_add.loc[
+        (mis_add['chat'] == 'Case TAT') & (mis_add['Particular'] == 'Case TAT unique cases (Cases created Day-1)'),date
+        ] = tat_hours_sm_4
+
+        mis_add.loc[
+        (mis_add['chat'] == 'Repeat') & (mis_add['Particular'] == 'Repeat (Customer Message Vs Case ID)'),'Target'
+        ] = 'Ratio-"1:4"'
+
+
+    # Showing the data frame
+    with c2:
+        st.dataframe(Inflow_hourly_count)
+    with c3:
+        st.dataframe(mis_add)
     st.write('Thanks for using, have greate day 😊')
 
